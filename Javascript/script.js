cfe185ecb3ac4a2107ba8814181cad8e98a10de2
Antonio Aldacélio Cavalcante <82114921+Aldacelio@users.noreply.github.com>
2022-10-22 16:15:07 -0300

    Login jogo da memoria

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+const jogador1 = document.getElementById('jogador1');
+const jogador2 = document.getElementById('jogador2');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login_form');
+
+function validateInput(){
+    if(jogador1.value.length && jogador2.value.length ){
+        button.removeAttribute('disabled');
+    }else{
+        button.setAttribute('disabled', '');
+    }
+}
+
+const handleSubmit = (event) =>{
+    event.preventDefault();
+    localStorage.setItem('Jogador1', jogador1.value);
+    localStorage.setItem('Jogador2', jogador2.value);
+
+    window.location = 'Pages/jogoMemoria.html';
+}
+
+jogador1.addEventListener('input', validateInput);
+jogador2.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
+
